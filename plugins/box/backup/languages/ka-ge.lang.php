1268eace22fd9ec10eebcb83202b1322f6469a72
mapadesign <panic.marjan@gmail.com>
2015-11-10 20:10:56 +0100

    Create ka-ge.lang.php

---
+++ plugins/box/backup/languages/ka-ge.lang.php
@@ -0,0 +1,21 @@
+<?php
+
+    return array(
+        'backup' => array(
+            'Backups' => 'Backup-ები',
+            'Backup' => 'Backup-ი',
+            'Create Backup' => 'Backup-ის შექმნა',
+            'Delete' => 'წაშლა',
+            'storage' => 'მონაცემები',
+            'public' => 'საჯარო',
+            'plugins' => 'პლაგინები',
+            'Size' => 'ზომა',
+            'Actions' => 'მოქმედება',
+            'Delete backup: :backup' => 'Backup-ი წაშლილია: :backup',
+            'Creating...' => 'მიმდენარეობს შექმნა...',
+            'Backup was deleted' => 'Backup-ი წაიშალა',
+            'Backup was created' => 'Backup-ი შეიქმნა',
+            'Backup was not restored' => 'Backup-ი ვერ აღადგინდა',
+            'Backup was restored' => 'Backup-ი აღადგინებულია',
+        )
+    );
