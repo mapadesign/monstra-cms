be04c979ffd38b1127963a44b7b9f9e5c9ca6477
mapadesign <panic.marjan@gmail.com>
2015-11-10 20:09:31 +0100

    Create ka-ge.lang.php

---
+++ plugins/blog/languages/ka-ge.lang.php
@@ -0,0 +1,12 @@
+<?php
+
+    return array(
+        'blog' => array(
+            'Blog' => 'ბლოგი',
+            'Blog plugin for Monstra' => 'ბლოგის პლაგინი Monstra-თვის',
+            'begin' => 'დასაწყისი',
+            'end' => 'ბოლო',
+            'prev' => 'უკან',
+            'next' => 'შემდეგი',
+        ) 
+    );
